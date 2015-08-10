8d58fe097b6806bd5fa3b36cc3145d55641af3bf
Artorias91 <m.contini91@gmail.com>
2015-08-10 17:33:11 +0200

    primo commit

--- PizzaClick/php/Settings.php
+++ PizzaClick/php/Settings.php
@@ -9,12 +9,16 @@ class Settings {
 
     // variabili di accesso per il database
     public static $db_host = 'localhost';
-//    public static $db_user = 'continiMattia';
-    public static $db_user = 'root';
-//    public static $db_password = 'elefante984';
-    public static $db_password = 'davide';
-//    public static $db_name='amm14_continiMattia';
-    public static $db_name='continiMattia';
+    
+    //server
+    public static $db_user = 'continiMattia';
+    public static $db_password = 'elefante984';
+    public static $db_name='amm14_continiMattia';
+
+    //locale
+//    public static $db_user = 'root';
+//    public static $db_password = 'davide';    
+//    public static $db_name='continiMattia';
     
     private static $appPath;
 
@@ -29,11 +33,11 @@ class Settings {
             switch ($_SERVER['HTTP_HOST']) {
                 case 'localhost':
                     // configurazione locale
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/Progetto-AMM/PizzaClick/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/PizzaClick/';
                     break;
                 case 'spano.sc.unica.it':
 //                    // configurazione pubblica
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/continiMattia/PizzaClick/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/continiMattia/';
                     break;
 
                 default:
