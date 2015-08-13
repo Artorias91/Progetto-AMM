3617ffb89aa41bc83e4b67ce70f500a5479b8fb4
Artorias91 <m.contini91@gmail.com>
2015-08-13 15:31:47 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,15 +1,4 @@
 <?php 
-  $delete = "rm -r /home/amm/repoAmm/amm2014/continiMattia/";
-  echo shell_exec($delete . "README.md");
-  echo shell_exec($delete . "Settings.php");
-  echo shell_exec($delete . "index.php");
-  echo shell_exec($delete . "pizzaClick.sql"); 
-  echo shell_exec($delete . "amm14Application");
-  echo shell_exec($delete . "css"); 
-  echo shell_exec($delete . "img"); 
-  echo shell_exec($delete . "js");
-  echo shell_exec($delete . "lib"); 
-  echo shell_exec($delete . "nbproject"); 
-  echo shell_exec($delete . "php");
-  echo shell_exec($delete . ".htaccess");
+  $comando = "ls -a /home/amm/repoAmm/amm2014/continiMattia/";
+  echo shell_exec($comando);
 ?>
